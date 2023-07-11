e3698b6bd345e0fe46a8b10c5ef82817b291f810
Andrew Plotkin <zarf@ZarfLent.local>
2023-07-11 12:14:49 -0400

    Require a script.

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,11 @@ pub struct AppOptions {
 fn main() {
     let opts = AppOptions::parse_args_default_or_exit();
 
+    if opts.script.len() == 0 {
+        println!("Usage: postim [OPTIONS] script");
+        std::process::exit(1);
+    }
+
     run::run(&opts).unwrap_or_else(|err| {
         println!("Error: {err}");
     });
