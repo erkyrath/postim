420bc5f8865d0856535e12c573795910ae078be2
Andrew Plotkin <zarf@ZarfLent.local>
2023-07-11 15:17:06 -0400

    Error handling in a simpler way.

--- src/script/parse.rs
+++ src/script/parse.rs
@@ -1,4 +1,3 @@
-use std::error::Error;
 use nom::IResult;
 use nom::error::ParseError;
 
@@ -17,8 +16,9 @@ pub fn parse_comment<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a 
 }
 
 
-pub fn load_script(filename: &str) -> Result<(), Box<dyn Error>> {
-    let body = std::fs::read_to_string(filename)?;
+pub fn load_script(filename: &str) -> Result<(), String> {
+    let body = std::fs::read_to_string(filename)
+        .map_err(|err| err.to_string())?;
 
     let res = parse_comment::<nom::error::VerboseError<&str>>(&body);
     // res: Result<(&str, ()), nom::Err<VerboseError<&str>>>
@@ -27,6 +27,7 @@ pub fn load_script(filename: &str) -> Result<(), Box<dyn Error>> {
         if let nom::Err::Error(verberr) = err {
             let errstr = nom::error::convert_error::<&str>(&body, verberr);
             println!("{filename}: {errstr}");
+            return Err("BAD".to_string());
         }
     }
 
