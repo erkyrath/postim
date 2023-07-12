896b376306ecfa2cc04d15671b5d6eb3b33cf2f1
Andrew Plotkin <zarf@ZarfLent.local>
2023-07-12 16:09:46 -0400

    Simplify.

--- src/run.rs
+++ src/run.rs
@@ -8,8 +8,7 @@ use crate::script::parse::load_script;
 pub fn run(opts: &AppOptions) -> Result<(), Box<dyn Error>> {
 
     let iter = opts.script.iter().map(|filename| load_script(&filename));
-    let res: Result<Vec<Script>, _> = iter.collect();
-    let scripts = res?;
+    let scripts: Vec<Script> = iter.collect::<Result<Vec<_>, _>>()?;
     
     println!("### loaded {} scripts", scripts.len());
     
